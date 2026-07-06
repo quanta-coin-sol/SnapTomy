cbb703089d9cca034d2774eb3088e5ec599f92f3
danialoo <htmi-css@outlook.com>
2026-07-06 09:24:52 +0300

    Remove token file, add to gitignore

--- get_tg_id.py
+++
@@ -1,9 +0,0 @@
-import json, urllib.request
-
-token = "8822383019:AAFkgbR-Hs-6HrvBpVnjohZyqPEir4OjRlk"
-url = f"https://api.telegram.org/bot{token}/getUpdates"
-data = json.loads(urllib.request.urlopen(url).read())
-for m in data.get("result", []):
-    if "message" in m:
-        f = m["message"]["from"]
-        print(f"ID: {f['id']} - @{f.get('username', '?')}")
